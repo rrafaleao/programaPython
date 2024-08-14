f930905b17b5ce26755382b3ebd38fcff102caf4
RAFAEL CAMPERA DE LEAO <05163643088@senacrs.edu.br>
2024-08-14 19:59:00 -0300

    python

---
+++ main.py
@@ -0,0 +1,35 @@
+print("olá mundo")
+x = 30
+y = 3.14
+nome = "alice"
+ativo = True
+'''
+print(nome)
+print (x)
+print(y)
+print(ativo)
+'''
+'''
+idade = 20
+if idade >= 18 :
+    print("você é maior de idade")
+elif idade < 18 and idade > 12:
+    print("vocÊ é adolescente ")
+else:
+    print("você é criança")
+'''
+
+'''
+for i in range(10) :
+    print(i)
+'''
+
+def saudacao(nome):
+	print(f"Olá, {nome}!")
+
+saudacao("João")
+
+contador = 0
+while contador < 5:
+    print(contador)
+    contador += 1
